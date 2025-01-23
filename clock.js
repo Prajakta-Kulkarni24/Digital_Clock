9e0359a2e73ffe1d4f0779d36ea998c24a291193
Prajakta-Kulkarni24 <prajaktavk2004@gmail.com>
2025-01-23 19:07:11 +0530

    clock.js

--- clock.js
+++ clock.js
@@ -3,28 +3,23 @@
     http://www.sitepoint.com/create-jquery-digital-clock-jquery4u/
 */
 
-<script>
-    function updateClock()
-    {
-        var currentTime = new Date ( );
-        var currentHours = currentTime.getHours ( );
-        var currentMinutes = currentTime.getMinutes ( );
-        var currentSeconds = currentTime.getSeconds ( );
+function updateClock() {
+    var currentTime = new Date();
+    var currentHours = currentTime.getHours();
+    var currentMinutes = currentTime.getMinutes();
+    var currentSeconds = currentTime.getSeconds();
 
-        currentHours = ( currentHours < 10 ? "0" : "" ) + currentHours;
-        currentMinutes = ( currentMinutes < 10 ? "0" : "" ) + currentMinutes;
-        currentSeconds = ( currentSeconds < 10 ? "0" : "" ) + currentSeconds;
+    currentHours = (currentHours < 10 ? "0" : "") + currentHours;
+    currentMinutes = (currentMinutes < 10 ? "0" : "") + currentMinutes;
+    currentSeconds = (currentSeconds < 10 ? "0" : "") + currentSeconds;
 
-        var currentTimeString = currentHours + ":" + currentMinutes + ":" + currentSeconds;
+    var currentTimeString = currentHours + ":" + currentMinutes + ":" + currentSeconds;
 
-        $(".timer").html(currentTimeString);
+    $(".timer").html(currentTimeString);
+}
 
-     }
-
-    $(document).ready(function()
-    {
-       setInterval('updateClock()', 1000);
-    });
-</script>
+$(document).ready(function() {
+    setInterval(updateClock, 1000);
+});
 
 <span class="timer"></span>
