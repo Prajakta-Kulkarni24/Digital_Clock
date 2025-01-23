97360f6408de7269503a3eb6ff2e5bcf3e46f81a
Prajakta-Kulkarni24 <prajaktavk2004@gmail.com>
2025-01-23 18:26:54 +0530

    clock.js

---
+++ clock.js
@@ -0,0 +1,30 @@
+ /*
+    Create Your Own jQuery Digital Clock
+    http://www.sitepoint.com/create-jquery-digital-clock-jquery4u/
+*/
+
+<script>
+    function updateClock()
+    {
+        var currentTime = new Date ( );
+        var currentHours = currentTime.getHours ( );
+        var currentMinutes = currentTime.getMinutes ( );
+        var currentSeconds = currentTime.getSeconds ( );
+
+        currentHours = ( currentHours < 10 ? "0" : "" ) + currentHours;
+        currentMinutes = ( currentMinutes < 10 ? "0" : "" ) + currentMinutes;
+        currentSeconds = ( currentSeconds < 10 ? "0" : "" ) + currentSeconds;
+
+        var currentTimeString = currentHours + ":" + currentMinutes + ":" + currentSeconds;
+
+        $(".timer").html(currentTimeString);
+
+     }
+
+    $(document).ready(function()
+    {
+       setInterval('updateClock()', 1000);
+    });
+</script>
+
+<span class="timer"></span>
